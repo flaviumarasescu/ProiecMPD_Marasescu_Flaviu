33d57570f5c1bb18da8d679a62901b3334ef3987
flaviumarasescu <50264537+flaviumarasescu@users.noreply.github.com>
2021-01-11 18:39:22 +0200

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,7 +8,6 @@ namespace ProiecMPD_Marasescu_Flaviu.Models
     public class Category
     {
         public int ID { get; set; }
-        
         public string CategoryName { get; set; }
         public ICollection<FilmCategory> FilmCategories { get; set; }
     }
