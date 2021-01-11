5a026172db996681d1c18bee117cd6eea895727e
flaviumarasescu <50264537+flaviumarasescu@users.noreply.github.com>
2021-01-11 18:30:11 +0200

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,6 +8,7 @@ namespace ProiecMPD_Marasescu_Flaviu.Models
     public class Category
     {
         public int ID { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Acest câmp trebuie să conțină litere și spații și să aibă o lungime mai mare de 2 caracte"), Required, StringLength(50, MinimumLength = 2)]
         public string CategoryName { get; set; }
         public ICollection<FilmCategory> FilmCategories { get; set; }
     }
